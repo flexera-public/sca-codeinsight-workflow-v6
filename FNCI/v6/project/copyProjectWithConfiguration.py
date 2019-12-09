cd4ea9da6b7d5c5dad3a057ce72d034053081657
Sean Geary <sgeary@flexera.com>
2019-12-09 12:31:03 -0500

    Update with check for new project ownership and fail if invalid

--- FNCI/v6/project/copyProjectWithConfiguration.py
+++ FNCI/v6/project/copyProjectWithConfiguration.py
@@ -5,7 +5,7 @@ Created on Sep 21, 2019
 '''
 import logging
 import requests
-import json
+import sys
 
 import config
 
@@ -22,36 +22,36 @@ def create_cloned_project(teamName, projectName, authToken):
     headers = {'Content-Type': 'application/json', 'Authorization': authToken}  
     RESTAPI_URL = ID_ENDPOINT_URL + config.v6_projectTemplatename + "/" + teamName +"/" + projectName
     logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)  
-    response = requests.post(RESTAPI_URL, headers=headers)
+    
              
     try:
-        logger.debug(json.dumps(response.json(), indent=3))  
+        response = requests.post(RESTAPI_URL, headers=headers)
     except ValueError:
         # no JSON returned
         logger.debug(response)
         logger.debug(response.text)
-        return  "FAILURE"
+        
+    # Check the response code and go from there
+    if  response.status_code == 200:
+        
+        projectId = (response.json()["Content"])
+        logger.debug("     Project ID for v6 project %s is %s" %(projectName, projectId))
+        return projectId
     
-    # Now parse the results from the REST call
-    if "HttpStatusCode" in response.json():
-        HttpStatusCode = (response.json()["HttpStatusCode"]) 
+    elif response.status_code == 400:
+        logger.error("Error encountered while trying to clone template project")
+        logger.error("RESPONSE DETAILS: %s" %(response.json()["Error(s) "]))
+        print("Error encountered while trying to clone template project")
+        print(response.json()["Error(s) "])
+        print("Exiting script.....")
+        sys.exit()   
         
-        if  HttpStatusCode == 200:
-            
-            projectId = (response.json()["Content"])
-            logger.debug("     Project ID for v6 project %s is %s" %(projectName, projectId))
-            return projectId
+    
+    else:
+        # Unknown status code that needs to be investigated
+        logger.error("Unknown HttpStatusCode: %s" %response.json()["Error(s)"])
+        print("Unknown Error.  Please see log for details.....")   
         
-        elif HttpStatusCode == 400:
-            logger.error("HttpStatusCode: %s" %response.json()["Error(s) "])
-            print("Unknown Error.  Please see log for details.....")   
-            
-        
-        else:
-            # Unknown status code that needs to be investigated
-            logger.error("Unknown HttpStatusCode: %s" %response.json()["Error(s) "])
-            print("Unknown Error.  Please see log for details.....")   
-            
             
 
       
