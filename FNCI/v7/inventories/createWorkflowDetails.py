74165d459466dbdcf4349f03dc2932b38f45f852
Sean Geary <sgeary@flexera.com>
2020-02-24 12:28:52 -0500

    Update v7 inventory APIs for exception handling

--- FNCI/v7/inventories/createWorkflowDetails.py
+++ FNCI/v7/inventories/createWorkflowDetails.py
@@ -9,9 +9,21 @@ File : createWorkflowDetails.py
 '''
 import logging
 import requests
+import sys
+from flask import abort
 import config
 
 logger = logging.getLogger(__name__)
+
+#######################################################################
+# If the calling app is a flask app then we can use
+# the flask abort function to catch exceptions
+# so see if its defined in a common config file
+try: 
+    FLASKAPP = config.FLASKAPP
+except:
+    FLASKAPP = False
+#######################################################################
 
 FNCI_API = "FNCI Create Workflow Details API"
 ENDPOINT_URL = config.BASEURL + "inventories/"
@@ -53,39 +65,77 @@ def update_inventory_workflow_details(inventoryId, UPDATEDETAILS, authToken):
     # Make the REST API call with the project data           
     try:
         response = requests.post(RESTAPI_URL, data=updateBody, headers=headers)
-    except requests.exceptions.HTTPError as errh:
-        print ("HTTP Error:",errh)
-        
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:",errc)
+        response.raise_for_status()
+    except requests.exceptions.ConnectionError:
+        # Connection Error - Is the server up and running?
+        abort_message = FNCI_API + " - Error Connecting to FNCI Server - " +  (ENDPOINT_URL).split("codeinsight")[0] # Get rid of everything after codeinsight in url
+        logger.error("    %s" %(abort_message))
 
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
-        
-    except requests.exceptions.RequestException as err:
-        print ("Oops: Something Else",err) 
+        if FLASKAPP:         
+            # Using error code 500 (Internal Server Error) to cover connection errors
+            # in the flask apps
+            abort(500, FNCI_API + " - %s" %abort_message) 
+        else:
+            print(abort_message)
+            print("Is the FNCI server running?")
+            print("Exiting script")
+            sys.exit() 
+    except requests.exceptions.RequestException as e: # Catch the exception for the logs but process below
+        logger.error(e)
     
+
     # We at least received a response from FNCI so check the status to see
     # what happened if there was an error or the expected data
     if response.status_code == 200:
+        logger.debug("    Call to %s was successful." %FNCI_API)
         logger.info("Inventory Workflow Details Updated")
-
+    
     elif response.status_code == 400:
         # Bad Request
-        # Assume there is no task data for the project
-        logger.debug("Response code 400 - %s" %response.text)         
-        
+        logger.error("    %s - Error: %s -  Bad Request." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(400, FNCI_API + " - Bad Request - Look at debug log for more details") 
+        else:
+            print("%s - Error: %s -  Bad Request." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()   
+
     elif response.status_code == 401:
-        # Unauthorized
-        logger.debug("Response code 401 - %s" %response.text) 
-        
+        # Unauthorized Access
+        logger.error("    %s - Error: %s -  Authentication Failed: JWT token is not valid or user does not have correct permissions." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(401, FNCI_API + " - Authentication Failed: JWT token is not valid or user does not have correct permissions.")
+        else:
+            print("%s - Error: %s -  Authentication Failed: JWT token is not valid or user does not have correct permissions." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()   
+
     elif response.status_code == 404:
         # Not Found
-        logger.debug("Response code 404 - %s" %response.text)
-        
+        logger.error("    %s - Error: %s -  URL endpoint not found:  %s" %(FNCI_API, response.status_code,  RESTAPI_URL ))
+        if FLASKAPP:         
+            abort(400, FNCI_API + " - Bad Request - URL endpoint not found") 
+        else:
+            print("    %s - Error: %s -  URL endpoint not found:  %s" %(FNCI_API, response.status_code,  RESTAPI_URL ))
+            print("    Exiting script")
+            sys.exit()   
+
     elif response.status_code == 405:
         # Method Not Allowed
-        logger.debug("Response code 405 - %s" %response.text)
+        logger.error("    %s - Error: %s -  Method (GET/POST/PUT//DELETE/ETC) Not Allowed." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(405, FNCI_API + " - Method Not Allowed.")
+        else:
+            print("    %s - Error: %s -  Method (GET/POST/PUT//DELETE/ETC) Not Allowed." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()  
         
     elif response.status_code == 500:
-        logger.debug("Response code 500 - %s" %response.text)
+        # Internal Server Error
+        logger.error("    %s - Error: %s -  Internal Server Error." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(500, FNCI_API + " - Internal Server Error.")
+        else:
+            print("    %s - Error: %s -  Internal Server Error." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()  
--- FNCI/v7/inventories/updateInventory.py
+++ FNCI/v7/inventories/updateInventory.py
@@ -5,14 +5,25 @@ Created on Nov 13, 2019
 '''
 import logging
 import requests
-import json
-
+import sys
+from flask import abort
 import config
-
 
 logger = logging.getLogger(__name__)
 
-ENDPOINT_URL = config.BASEURL + "inventories"
+#######################################################################
+# If the calling app is a flask app then we can use
+# the flask abort function to catch exceptions
+# so see if its defined in a common config file
+try: 
+    FLASKAPP = config.FLASKAPP
+except:
+    FLASKAPP = False
+#######################################################################
+
+FNCI_API = "FNCI Update Inventory API"
+ENDPOINT_URL = config.BASEURL + "inventories/"
+
 
 #--------------------------------------------------------------------#
 def update_inventory_item_workflowURL(inventoryID, requestURL, authToken):
@@ -36,35 +47,87 @@ def update_inventory_item_UsageGuidance(inventoryID, guidanceText, authToken):
 def update_inventory_item(inventoryID, updateBody, authToken):
     logger.debug("Entering update_inventory_item with inventoryID %s" %inventoryID)
 
-      
     headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken}    
-    RESTAPI_URL = ENDPOINT_URL +  "/" + str(inventoryID)
+    RESTAPI_URL = ENDPOINT_URL + str(inventoryID)
     logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)  
-       
+               
+    #  Make the request to get the required data   
     try:
         response = requests.put(RESTAPI_URL, data=updateBody, headers=headers)
-        logger.debug(json.dumps(response.json(), indent=3))  
-        
-    except requests.exceptions.RequestException as e:
-        print(e)
-        logger.debug(e)
-        logger.debug(response)
-        logger.debug(response.text)
-        return False
+        response.raise_for_status()
+    except requests.exceptions.ConnectionError:
+        # Connection Error - Is the server up and running?
+        abort_message = FNCI_API + " - Error Connecting to FNCI Server - " +  (ENDPOINT_URL).split("codeinsight")[0] # Get rid of everything after codeinsight in url
+        logger.error("    %s" %(abort_message))
+
+        if FLASKAPP:         
+            # Using error code 500 (Internal Server Error) to cover connection errors
+            # in the flask apps
+            abort(500, FNCI_API + " - %s" %abort_message) 
+        else:
+            print(abort_message)
+            print("Is the FNCI server running?")
+            print("Exiting script")
+            sys.exit() 
+    except requests.exceptions.RequestException as e: # Catch the exception for the logs but process below
+        logger.error(e)
     
         
-    # Check the response code and proceed accordingly
+    # We at least received a response from FNCI so check the status to see
+    # what happened if there was an error or the expected data
     if response.status_code == 200:
-        logger.debug("Update succeeded")
+        logger.debug("    Call to %s was successful." %FNCI_API)
+    
+    elif response.status_code == 400:
+        # Bad Request
+        logger.error("    %s - Error: %s -  Bad Request." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(400, FNCI_API + " - Bad Request - Look at debug log for more details") 
+        else:
+            print("%s - Error: %s -  Bad Request." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()   
 
     elif response.status_code == 401:
-        print("Unauthorized")        
-    
+        # Unauthorized Access
+        logger.error("    %s - Error: %s -  Authentication Failed: JWT token is not valid or user does not have correct permissions." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(401, FNCI_API + " - Authentication Failed: JWT token is not valid or user does not have correct permissions.")
+        else:
+            print("%s - Error: %s -  Authentication Failed: JWT token is not valid or user does not have correct permissions." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()   
+
     elif response.status_code == 404:
-        print("Inventory with id of %s was not found" %inventoryID)
-    
+        # Not Found
+        logger.error("    %s - Error: %s -  URL endpoint not found:  %s" %(FNCI_API, response.status_code,  RESTAPI_URL ))
+        if FLASKAPP:         
+            abort(400, FNCI_API + " - Bad Request - URL endpoint not found") 
+        else:
+            print("    %s - Error: %s -  URL endpoint not found:  %s" %(FNCI_API, response.status_code,  RESTAPI_URL ))
+            print("    Exiting script")
+            sys.exit()   
+
+    elif response.status_code == 405:
+        # Method Not Allowed
+        logger.error("    %s - Error: %s -  Method (GET/POST/PUT//DELETE/ETC) Not Allowed." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(405, FNCI_API + " - Method Not Allowed.")
+        else:
+            print("    %s - Error: %s -  Method (GET/POST/PUT//DELETE/ETC) Not Allowed." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()  
+        
     elif response.status_code == 500:
-        print("Internal Server Error")
+        # Internal Server Error
+        logger.error("    %s - Error: %s -  Internal Server Error." %(FNCI_API, response.status_code ))
+        if FLASKAPP:         
+            abort(500, FNCI_API + " - Internal Server Error.")
+        else:
+            print("    %s - Error: %s -  Internal Server Error." %(FNCI_API, response.status_code ))
+            print("    Exiting script")
+            sys.exit()  
+        
         
         
 #--------------------------------------------------#        
