57657bca7894cd8b02b59827b72776e076258d1c
Sean Geary <sgeary@flexera.com>
2020-08-27 16:14:28 -0400

    Update config

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ FNCI_HOST = "localhost"
 FNCI_PORT = "8888"
 BASEURL = "http://" + FNCI_HOST + ":" + FNCI_PORT + "/codeinsight/api/"
 # Token for Workflow Admin User  ( must be added to project to update task)
-ADMIN_AUTHTOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsInVzZXJJZCI6MSwiaWF0IjoxNTgwMjE2MDg0fQ.rHPyLbJ24BZbmN3ng0WLJH2pKsQgv2aklaGs3jAJMYxKHKBm_Qp8J_WXL93s_sQIh_zynEKjGGIim_giKgXajQ"
-AUTHTOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvb3duZXIiLCJ1c2VySWQiOjYsImlhdCI6MTU4MDI5OTQzN30.LuxiDTgRSked1guyADNK3CMZIIDaUHDXwyTPLc_77rvhDW6d4GVUfK6SNXRntHZGpGScnzPoq9UeOzdjUbXyCQ"
+ADMIN_AUTHTOKEN = "v7 ADMIN JWT TOKEN"
+AUTHTOKEN = "v7 USER JWT TOKEN"
 
 v6_FNCI_HOST = "localhost"
 v6_FNCI_PORT = "8880"
@@ -22,9 +22,9 @@ v6_REQUESTURL = "http://" + v6_FNCI_HOST + ":" + v6_FNCI_PORT + "/palamida/Reque
 
 # Token for Workflow Admin User
 # This user must own the template project!
-v6_AUTHTOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3b3JrZmxvd2FkbWluIiwiaWF0IjoxNTc2NzYwNzAzfQ.3eIPBdUwapkw0kVyW1AatEkvlZt1ygiidGbIxFmSfTk"
-v6_teamName = "Engineering"
-v6_projectTemplatename = "Workflow Template"  # Must be owned by user with Auth Token above
+v6_AUTHTOKEN = "v6ADMIN JWT TOKEN"
+v6_teamName = "Engineering"  # The team that owns the template project to be copyied
+v6_projectTemplatename = "Workflow Template"  # The project template be owned by user with Auth Token above
 
 # To allow for changes in script that are release specific
-FNCI_VERSION = "2020R1"
+FNCI_VERSION = "2020R2"
